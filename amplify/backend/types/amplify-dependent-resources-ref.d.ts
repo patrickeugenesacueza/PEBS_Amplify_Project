ef033c250a5f39cdf018119936d70bd5e2b34a6d
patrickeugenesacueza <127990921+patrickeugenesacueza@users.noreply.github.com>
2023-04-10 20:48:47 +0800

    added auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "pebsamplifyproject3f8094a9": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
